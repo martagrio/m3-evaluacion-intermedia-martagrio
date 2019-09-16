f5d737e47daa5d0ffbdb8ebc88fa6dfa76e597fc
martagrio <marta.igr90@gmail.com>
2019-09-16 17:08:19 +0200

    Changes in files

---
+++ docs/precache-manifest.97bd3fcf7c160c41607a2a77c24a19b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bec98ce9e3218b8c6675dc744868566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9939104d03614c6e1f5a",
+    "url": "./static/css/main.130c1026.chunk.css"
+  },
+  {
+    "revision": "94c0e5cbf7fe33d30cfd",
+    "url": "./static/js/2.5f063442.chunk.js"
+  },
+  {
+    "revision": "9939104d03614c6e1f5a",
+    "url": "./static/js/main.54568de6.chunk.js"
+  },
+  {
+    "revision": "751979b37b44c485cf3b",
+    "url": "./static/js/runtime~main.147ee3e8.js"
+  }
+]);
