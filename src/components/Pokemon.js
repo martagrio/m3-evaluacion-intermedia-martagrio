574d562c6d79f6dba6b50a021260fed97cfbe675
martagrio <marta.igr90@gmail.com>
2019-09-16 19:20:03 +0200

    Still working on css

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -7,16 +7,14 @@ class Pokemon extends React.Component {
 		return(
 			<div className="card-pokemon">
 				<img src={this.props.pic} alt={this.props.name} className="card-pokemon-img"/>
-				<div className="card-pokemon-content">
-					<h2 className="card-pokemon__name">{this.props.name}</h2>
-					<ul className="card-pokemon__types">
-						{this.props.type.map((type, id) => {
-              return (
-                <li className="card-pokemon__type" key={id}>{type}</li>
-              );
-            })}
-					</ul>
-				</div>
+				<h2 className="card-pokemon__name">{this.props.name}</h2>
+				<ul className="card-pokemon__types">
+					{this.props.type.map((type, id) => {
+						return (
+							<li className="card-pokemon__type" key={id}>{type}</li>
+						);
+					})}
+				</ul>
 			</div>
 		)
 	}
